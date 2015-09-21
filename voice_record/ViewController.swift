a9071302a7a99ea71fcb435a0413637119c35bbf
msouth6 <matthewsouthwell13@gmail.com>
2015-09-21 19:20:59 +0900

    Second view added.  End of lesson 3a

--- voice_record/ViewController.swift
+++ voice_record/ViewController.swift
@@ -11,11 +11,18 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordLabel: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewWillAppear(animated: Bool) {
+        stopButton.hidden=true
+        recordButton.enabled=true
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -23,6 +30,8 @@ class ViewController: UIViewController {
 
     @IBAction func recordAudio(sender: UIButton) {
         recordLabel.hidden=false
+        stopButton.hidden=false
+        recordButton.enabled=false
         print("in recordAudio")
     }
 
