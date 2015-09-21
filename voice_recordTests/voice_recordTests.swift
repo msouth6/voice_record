4dfecf847e122de2052402f86673f3e31509e711
msouth6 <matthewsouthwell13@gmail.com>
2015-09-21 18:49:24 +0900

    Initial Commit

---
+++ voice_recordTests/voice_recordTests.swift
@@ -0,0 +1,36 @@
+//
+//  voice_recordTests.swift
+//  voice_recordTests
+//
+//  Created by Matt Southwell on 9/21/15.
+//  Copyright © 2015 Matt Southwell. All rights reserved.
+//
+
+import XCTest
+@testable import voice_record
+
+class voice_recordTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
