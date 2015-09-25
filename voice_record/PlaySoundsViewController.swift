60e62e0a9d586c9eff30edaedf3bf5a020ab6589
msouth6 <matthewsouthwell13@gmail.com>
2015-09-25 22:09:42 +0900

    End of Lesson 3

---
+++ voice_record/PlaySoundsViewController.swift
@@ -0,0 +1,62 @@
+//
+//  PlaySoundsViewController.swift
+//  voice_record
+//
+//  Created by Matt Southwell on 9/21/15.
+//  Copyright © 2015 Matt Southwell. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+
+    var audioPlayer:AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let fileUrl = NSBundle.mainBundle().URLForResource("movie_quote", withExtension: "mp3"){
+            audioPlayer = try!AVAudioPlayer(contentsOfURL: fileUrl)
+            audioPlayer.enableRate=true
+            print("Loaded the audio")
+        }else {
+            print("Movie Quote Not Found")
+        }
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func playAudio(speed: Float){
+        audioPlayer.stop()
+        audioPlayer.rate = speed
+        audioPlayer.currentTime=0.0
+        audioPlayer.play()
+    }
+    
+    @IBAction func playFastAudio(sender: UIButton) {
+        playAudio(1.5)
+    }
+    @IBAction func playSlowAudio(sender: UIButton) {
+        playAudio(0.5)
+    }
+
+    @IBAction func stopAudio(sender: UIButton) {
+        audioPlayer.stop()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
