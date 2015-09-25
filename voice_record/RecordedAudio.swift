096aa4d1b5530a85e83e23ce78eeff549652b505
msouth6 <matthewsouthwell13@gmail.com>
2015-09-25 23:22:09 +0900

    -Added RecordedAudio file

---
+++ voice_record/RecordedAudio.swift
@@ -0,0 +1,15 @@
+//
+//  RecordedAudio.swift
+//  voice_record
+//
+//  Created by Matt Southwell on 9/25/15.
+//  Copyright © 2015 Matt Southwell. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+    
+}
